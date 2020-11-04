c831b82d3d0eaaeef3cfc0d29351aad57f70dc60
Lafen Lesley <lesleytech6@gmail.com>
2020-11-04 14:17:51 +0200

    Build app

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,9 +3,7 @@ import React, { createContext, useState } from 'react';
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem('authenticated_user'))
-  );
+  const [currentUser, setCurrentUser] = useState(null);
 
   return (
     <AuthContext.Provider value={[currentUser, setCurrentUser]}>
